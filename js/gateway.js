f21b1dab3a82aae5ad9d5499549ef92acf583dba
lunalauti <lauluna817@gmail.com>
2023-11-01 13:15:18 -0300

    selector de piso, solicitud de datos y ascensores

--- js/gateway.js
+++ js/gateway.js
@@ -1,43 +1,3 @@
-// const http = require('http');
-
-// const gatewayPort = 3000;
-
-// const gatewayServer = http.createServer((req, res) => {
-//   if (req.url === '/solicitud_acceso') {
-//     // Enrutar la solicitud al Microservicio 1
-//     const options = {
-//       hostname: 'localhost',
-//       port: 4000, // Puerto del Microservicio 1
-//     //   path: '/selector_pisos.js', // Ruta específica del Microservicio 1
-//       method: req.method,
-//     //   headers: req.headers
-//     };
-
-//     const proxyRequest = http.request(options, (proxyResponse) => {
-//       res.writeHead(proxyResponse.statusCode, proxyResponse.headers);
-//       proxyResponse.pipe(res, {
-//         end: true
-//       });
-//     });
-
-//     req.pipe(proxyRequest, {
-//       end: true
-//     });
-//   } else {
-//     // Ruta no válida
-//     res.statusCode = 404;
-//     res.end('Ruta no encontrada');
-//   }
-// });
-
-// gatewayServer.listen(gatewayPort, () => {
-//   console.log(`Gateway escuchando en el puerto ${gatewayPort}`);
-// });
-
-
-// HTTP server
-// server.js
-
 const http = require('http');
 const path = require('path');
 const { send } = require('process');
@@ -55,39 +15,16 @@ const server = http.createServer(function (request, response) {
     request.on('data', (chunk) => {
       body += chunk;
     });
-
     request.on('end', () => {
-      console.log('3) Server received: ' + body);
-      console.log('4) Write: Mundo');
-      console.log('5) URL = '+ request.url);
-
+      console.log('3 Gateway');
+      console.log('4) Server received: ' + body);
+      console.log('5) Write: Mundo');
+      console.log('6) URL = '+ request.url);
       // URL del selector de pisos
       const url = 'http://localhost:4000/solicitud_acceso';
       const path = '/solicitud_acceso';
 
       send_request(url,'POST',body);
-
-      // fetch(url, {
-      //   method: 'POST',
-      //   body: body,
-      //   path : JSON.stringify(path),
-      // })
-      //   .then(response => {
-      //     if (response.ok) {
-      //       return response.json();
-      //     } else {
-      //       throw new Error('Error en la solicitud');
-      //     }
-      //   })
-      //   .then(data => {
-      //     console.log('Respuesta del servidor:', data);
-      //     // Realiza la lógica necesaria con la respuesta del servidor
-      //   })
-      //   .catch(error => {
-      //     console.error('Error:', error);
-      //   });
-        
-      //Debe mandar los datos correspondientes
       response.end('Gateway1');
       
     });
@@ -96,7 +33,7 @@ const server = http.createServer(function (request, response) {
       console.log('6) Socket closed');
     });
 
-  } else if (request.url === '/consulta_piso'){
+  } else if (request.url === '/consulta_datos'){
       let body = '';
       request.on('data', (chunk) => {
       body += chunk;
@@ -107,34 +44,9 @@ const server = http.createServer(function (request, response) {
       console.log('5) URL = '+ request.url)
 
       // URL de consulta de piso
-      const url = 'http://localhost:4000/consulta_piso';
-      const path = '/consulta_piso'
+      const url = 'http://localhost:4000/consulta_datos';
 
       send_request(url,'POST',body);
-
-      // fetch(url, {
-      //   method: 'POST',
-      //   body: body,
-      //   path : JSON.stringify(path),
-      // })
-      //   .then(response => {
-      //     if (response.ok) {
-      //       return response.json();
-      //     } else {
-      //       throw new Error('Error en la solicitud');
-      //     }
-      //   })
-      //   .then(data => {
-      //     console.log('Respuesta del servidor:', data);
-      //     responseData = data; // Almacena los datos en la variable responseData
-      //   })
-      //   .catch(error => {
-      //     console.error('Error:', error);
-      //   })
-      //   .finally(() => {
-      //     // Envía la respuesta al cliente
-      //     response.end(JSON.stringify(responseData)); // Envía la respuesta al cliente
-      //   });
     });
 
     request.on('close', () => {
--- js/selector_pisos.js
+++ js/selector_pisos.js
@@ -1,3 +1,6 @@
+const http = require('http');
+const URL_PERMISOS = 'http://localhost:8080/'
+const URL_ASCENSOR = 'http://localhost:8080/'
 //A modo de prueba agregamos unos JSON con datos de visitantes
 const datos = `[
     {
@@ -31,31 +34,29 @@ const datos = `[
   
 const data = JSON.parse(datos);
 
-const http = require('http');
-const path = require('path');
+
 
 const server = http.createServer(function (request, response) {
-
-  // Configuración de CORS------ que onda esto??
-  // response.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500');
-  // response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  // response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
     if (request.url === '/solicitud_acceso'){
         let body = '';
+        
         request.on('data', (chunk) => {
           body += chunk;
         });
-        request.on('end', () => {
-            
-          console.log('3) Selector_Pisos recibe: ' + body)
-          console.log('4) URL = '+ request.url)
 
+        request.on('end', () => {
+          try{
+            request_data = JSON.parse(body)
+            resp = solicitar_acceso(request_data)
+            response.statusCode= resp[0]
+            response.end(JSON.stringify(resp[1]))
+          }catch(error){
+            response.statusCode = 400
+            response.end('Error en los datos JSON de la solicitud')
+          }
         });
-        request.on('close', () => {
-            console.log('5) Socket closed');
-          });
-    } else if (request.url === '/consulta_piso'){
+
+    } else if (request.url === '/consulta_datos'){
         let body = '';
         request.on('data', (chunk) => {
           body += chunk;
@@ -74,28 +75,77 @@ const server = http.createServer(function (request, response) {
             response.end(JSON.stringify(objetoEncontrado))
   
           });
-          request.on('close', () => {
-              console.log('5) Socket closed');
-            });
+
     }else {
         response.statusCode = 404;
         response.end('Ruta no encontrada');
       }
 
 });
-
-
-
-server.listen(4000, function() {
-    console.log('1) Server started');
-  });
   
-
-function acceso(request){
-
+function validar_permisos(request, datos){
+  //verifica que el piso solicitado este en los autorizados
+  return true
 }
 
-function busco_datos(data,objeto) {
-    //Para buscar 
- }
+function solicitar_datos(data){
+  const id = {"id": data.id}
+  let datos = ''
+  
+  const request = http.request(URL_PERMISOS,{ method: 'GET' },
+    function (response) {
+      let body = ''
+      
+      response.on('data', (chunk) => {
+        body += chunk;
+      });
 
+      response.on('end', () => {
+        datos = body
+      });
+    });
+
+    request.write(JSON.stringify(id));
+    request.end();
+
+  return datos
+}
+
+function solicitar_acceso(request_data){
+  respuesta = []
+  datos = solicitar_datos(request_data) 
+  if (validar_permisos(request_data, datos)){
+    respuesta[0] = 200
+    respuesta[1] = solicitar_ascensor(request_data)
+  }
+  else{
+    respuesta[0] = 403
+    respuesta[1] = ""
+  }
+  return respuesta
+}
+
+function solicitar_ascensor(data){
+  const piso = {"piso": data.piso}
+  let ascensor = ''
+
+  const request = http.request(URL_ASCENSOR,{ method: 'POST' },
+    function (response) {
+      let body = ''
+      
+      response.on('data', (chunk) => {
+        body += chunk;
+      });
+
+      response.on('end', () => {
+        ascensor = body
+      });
+    });
+    request.write(JSON.stringify(piso));
+    request.end();
+  return ascensor
+}
+
+server.listen(4000, function() {
+  console.log('1) Server started');
+});
