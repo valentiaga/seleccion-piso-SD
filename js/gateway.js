1ace91d8080015afaf9fc2b26c31ddc9b89f5851
valentina reale <valenreale123@gmail.com>
2023-11-14 12:55:18 -0300

    gateway eleccion de ascensor

--- js/gateway.js
+++ js/gateway.js
@@ -26,86 +26,105 @@ const server = http.createServer(function (req, res) {
     return res.end();
   }
 
-  console.log('URL recibido '+req.url)
-  const parsedUrl =  req.url.slice(1).split('/')
+  console.log('URL recibido ' + req.url)
+  const parsedUrl = req.url.slice(1).split('/')
 
-  if (req.method=='GET' && parsedUrl.at(-1) === 'acceso'){
+  if (req.method == 'GET' && parsedUrl.at(-1) === 'acceso') {
     console.log("entra a gatoway");
-    request_data = { id: parsedUrl[1] } 
+    request_data = { id: parsedUrl[1] }
     acceder(request_data)
-    .then((resp) => {
-      let respuesta = resp[0]
-      let status = resp[1]
-      
-      console.log('manda la data bien ' + respuesta)
-      console.log('status '+ status)
-      
-      res.statusCode = status
-      res.end(JSON.stringify(respuesta))
-    })
-    .catch((error) => {
-      console.log("Error " + error.message)
-      res.statusCode = error.status
-      // res.statusCode = error.status
-      res.end('Error ' + error.status)
-    })
+      .then((resp) => {
+        let respuesta = resp[0]
+        let status = resp[1]
 
-  } else if (req.method=='GET' && parsedUrl.at(-1) === 'info'){
-      const id = parsedUrl[1]
-      console.log('3) Server received: ' + id)
-      console.log('4) URL = '+ req.url)
+        console.log('manda la data bien ' + respuesta)
+        console.log('status ' + status)
 
-      const url = 'http://localhost:4000/visitantes/'+ id +'/ascensor'
+        res.statusCode = status
+        res.end(JSON.stringify(respuesta))
+      })
+      .catch((error) => {
+        console.log("Error " + error.message)
+        res.statusCode = error.status
+        // res.statusCode = error.status
+        res.end('Error ' + error.status)
+      })
 
-    const request = http.request(url, { method: req.method },
-      function (response) {
-        let body = ''
-        console.log('entra 2')
-        response.on('data', (chunk) => {
-          body += chunk;
-        });
-  
-        response.on('end', () => {
-          console.log("El gateway recibe " + body)
-          request.end()
-          res.end(body); 
-        });
-        
-      });
+  } else if (req.method == 'GET' && parsedUrl.at(-1) === 'info') {
+    console.log("entra a gatoway");
+    request_data = { id: parsedUrl[1] }
+    acceder(request_data)
+      .then((resp) => {
+        let respuesta = resp[0]
+        let status = resp[1]
 
-    if( req.method != 'GET'){
-      console.log("El gateway envia" + data)
-      request.write(data);   
-    }
+        console.log('manda la data bien ' + respuesta)
+        console.log('status ' + status)
 
-    request.end()
-  
+        res.statusCode = status
+        res.end(JSON.stringify(respuesta))
+      })
+      .catch((error) => {
+        console.log("Error " + error.message)
+        res.statusCode = error.status
+        // res.statusCode = error.status
+        res.end('Error ' + error.status)
+      })
+    //     const id = parsedUrl[1]
+    //     console.log('3) Server received: ' + id)
+    //     console.log('4) URL = '+ req.url)
 
-      //habria que agregarle un catch por si tiene un error y que devuelva un codigo de error
-  }
-  
-});
+    //     const url = 'http://localhost:4000/visitantes/'+ id +'/ascensor'
 
-server.listen(3000, function() {
+    //   const request = http.request(url, { method: req.method },
+    //     function (response) {
+    //       let body = ''
+    //       console.log('entra 2')
+    //       response.on('data', (chunk) => {
+    //         body += chunk;
+    //       });
+
+    //       response.on('end', () => {
+    //         console.log("El gateway recibe " + body)
+    //         request.end()
+    //         res.end(body); 
+    //       });
+
+    //     });
+
+    //   if( req.method != 'GET'){
+    //     console.log("El gateway envia" + data)
+    //     request.write(data);   
+    //   }
+
+    //   request.end()
+
+
+    //     //habria que agregarle un catch por si tiene un error y que devuelva un codigo de error
+    // }
+
+  });
+
+server.listen(3000, function () {
   console.log('1) Server started');
 });
 
-async function acceder (request_data){
-  
+async function acceder(request_data) {
+
   console.log('Entra a Acceder')
   let url = URL_SELECTOR + '/visitantes/' + request_data.id + '/acceso'
 
   let respInfo = await send_request({ url: url, method: 'GET' })
   let datos = respInfo.body
-  console.log("Acceder recibe"+ respInfo.statusCode)
+  console.log("Acceder recibe" + respInfo.statusCode)
 
-    if (respInfo.statusCode != 200){
-      throw new Error_request(respInfo.statusCode)
-    }
-    let status = respInfo.statusCode
-    console.log("Acceder recibe: " + JSON.stringify(datos)) 
-    
-    return [datos, status];
+  if (respInfo.statusCode != 200) {
+    throw new Error_request(respInfo.statusCode)
+  }
+  let status = respInfo.statusCode
+  console.log("Acceder recibe: " + JSON.stringify(datos))
+
+  return [datos, status];
 }
 
 function send_request({ data, url, method } = {}) {
@@ -124,7 +143,7 @@ function send_request({ data, url, method } = {}) {
           body = JSON.parse(body);
           //estado = stringify(statusCode)
           // console.log("el selector recibe un codigo de " +response.statusCode)
-          resolve({"body":body, "statusCode":response.statusCode})
+          resolve({ "body": body, "statusCode": response.statusCode })
         });
       });
 
@@ -146,7 +165,7 @@ function send_request({ data, url, method } = {}) {
 //         response.on('data', (chunk) => {
 //           body += chunk;
 //         });
-  
+
 //         response.on('end', () => {
 //           console.log("El gateway recibe " + body)
 //           resolve(body)
@@ -163,7 +182,7 @@ function send_request({ data, url, method } = {}) {
 
 class Error_request extends Error {
   constructor(status) {
-      super(mensaje);
-      this.status = status;
+    super(mensaje);
+    this.status = status;
   }
 }
